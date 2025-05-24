b660192fa5b4208f84310e73e786c2a8cb50a683
Summer Salad <summer.salad.games@gmail.com>
2025-05-24 12:38:06 +0200

    Better button.py

--- pico/src/modules/button.py
+++ pico/src/modules/button.py
@@ -5,7 +5,7 @@ import time
 
 class Button:
 
-    def __init__(self, pin=8, debounce=250):
+    def __init__(self, pin=3, debounce=250):
         self._pin = pin
         self._debounce = debounce
         self._callback = None
@@ -13,14 +13,17 @@ class Button:
         self._ready = True
         self._last_check = -1
 
-        self._button_pin.irq(trigger=Pin.IRQ_RISING, handler=self._update_ready)
-        self._button_pin.irq(trigger=Pin.IRQ_FALLING, handler=self._debounce_handler)
-
         print("Setup Buttton complete")
+
+    def loop(self):
+        if not self._ready and self._button_pin.value() == 1:
+            self._ready = True
 
     @property
     def is_pressed(self):
         if not self._ready:
+            if self._button_pin.value() == 1:
+                self._ready = True
             return False
 
         if self._button_pin.value() == 0 and time.ticks_diff(time.ticks_ms(), self._last_check) >= self._debounce:
@@ -29,17 +32,3 @@ class Button:
             return True
 
         return False
-
-    def _debounce_handler(self, pin):
-        current_time = time.ticks_ms()
-        if time.ticks_diff(current_time, self._last_check) >= self._debounce:
-            self._last_check = current_time
-            self._ready = False
-            if self._callback:
-                self._callback()
-
-    def on_press(self, callback):
-        self._callback = callback
-
-    def _update_ready(self, pin):
-        self._ready = True
